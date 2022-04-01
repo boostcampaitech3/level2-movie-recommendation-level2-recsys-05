618b6826cc05950a7322ff89b361159019317ec4
kidjung <kjpark4321@naver.com>
2022-04-01 08:17:48 +0000

    add : baseline code v4 기본 설계를 위한 간단한 디렉토리 설정
    조만간 issue란을 이용하여 설계 명세를 진행하도록 하겠습니다.

---
+++ Baseline_Code_V4/experiment.py
@@ -0,0 +1,7 @@
+class Experiment:
+    def __init__(self, args, margs) -> None:
+        self.trainer = None
+        pass
+
+    def run(self) -> None:
+        pass
--- Baseline_Code_V4/utils.py
+++ Baseline_Code_V4/utils.py
@@ -1,3 +1,12 @@
+import torch
+import numpy as np
+import random
+import glob
+import re
+
+from pathlib import Path
+
+
 class dotdict(dict):
     """_summary_
 
@@ -10,3 +19,42 @@ class dotdict(dict):
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
+
+
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+
+
+def divide_except_zero(x, y):
+    try:
+        return x / y
+    except BaseException:
+        return "X"
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group["lr"]
+
+
+def increment_path(path, exist_ok=False):
+    """Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
+    Args:
+        path (str or pathlib.Path): f"{model_dir}/{args.name}".
+        exist_ok (bool): whether increment path (increment if False).
+    """
+    path = Path(path)
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}*")
+        matches = [re.search(rf"%s(\{d}+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]
+        n = max(i) + 1 if i else 2
+        return f"{path}{n}"
