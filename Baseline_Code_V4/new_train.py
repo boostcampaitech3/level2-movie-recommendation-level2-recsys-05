de52e4e47910b2024d56b7ac02fd0016dfd77837
yuujj <yujj0108@gmail.com>
2022-04-06 10:56:09 +0000

    feat: MLFlow 기능 추가
    - experiment, user, runname tagging
    - args, margs Artifacts로 관리
    - model, metric 관리

--- Baseline_Code_V4/new_train.py
+++ Baseline_Code_V4/new_train.py
@@ -4,6 +4,8 @@ from utils import seed_everything
 import yaml
 
 from experiment import Experiment
+import mlflow
+import mlflow.pytorch
 
 if __name__ == "__main__":
 
@@ -24,7 +26,8 @@ if __name__ == "__main__":
     with open(f"./model_config/{args.model_config}.yaml") as f:
         tmp_args = yaml.load(f, Loader=yaml.FullLoader)
         margs = dotdict(tmp_args)
-
+    margs_artifacts = dict(margs)
+    
     print("-" * 20, "model arguments list", "-" * 20)
     for k, v in margs.items():
         print(k, ":", v)
@@ -34,4 +37,19 @@ if __name__ == "__main__":
 
     experiment = Experiment(args, margs)
 
-    experiment.run()
+    # -- mlflow
+    remote_server_uri = "http://101.101.211.226:30005"
+    mlflow.set_tracking_uri(remote_server_uri)
+    mlflow_experiment = mlflow.get_experiment_by_name(args.experiment_name)
+    client = mlflow.tracking.MlflowClient()
+    
+    run = client.create_run(mlflow_experiment.experiment_id)
+    
+    with mlflow.start_run(run_id=run.info.run_id):
+        mlflow.set_tag("mlflow.user", args.user)
+        mlflow.set_tag("mlflow.runName", args.run_name)
+        
+        mlflow.log_dict(dict(args), args.run_name+"_args.yaml")
+        mlflow.log_dict(dict(margs_artifacts), args.run_name+"_margs.yaml")
+        
+        experiment.run()
