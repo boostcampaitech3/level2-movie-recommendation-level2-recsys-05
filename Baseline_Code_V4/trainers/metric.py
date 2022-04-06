1cb4ababe509eefb0ab83be7b1bd6238ac1e263f
yuujj <yujj0108@gmail.com>
2022-04-06 06:32:51 +0000

    chore: #13 그래프 기반 ndcg metric 추가

--- Baseline_Code_V4/trainers/metric.py
+++ Baseline_Code_V4/trainers/metric.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import torch
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def get_idcg(pred_list):
     idcg = sum((1 / np.log2(rank + 2) for rank in range(1, len(pred_list))))
@@ -23,3 +24,17 @@ def get_hit(pred_list, true_list):
     hit_list = set(true_list) & set(pred_list)
     hit = len(hit_list) / len(true_list)
     return hit
+
+
+def get_graph_ndcg(pred_items, test_items, test_indices, k):
+    """그래프는 배치단위로 계산해야하기 때문에 따로 함수 지정
+    """    
+    r = (test_items * pred_items).gather(1, test_indices)
+    f = torch.from_numpy(np.log2(np.arange(2, k + 2))).float().to(device)
+
+    dcg = (r[:, :k] / f).sum(1)
+    dcg_max = (torch.sort(r, dim=1, descending=True)[0][:, :k] / f).sum(1)
+    ndcg = dcg / dcg_max
+
+    ndcg[torch.isnan(ndcg)] = 0
+    return ndcg
