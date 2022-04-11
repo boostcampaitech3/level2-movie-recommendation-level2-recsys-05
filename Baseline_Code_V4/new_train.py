2965399ae33907428bd89d71752ac9474f87fd0d
yuujj <yujj0108@gmail.com>
2022-04-11 12:50:22 +0000

    chore: #13 inference mode 추가

--- Baseline_Code_V4/new_train.py
+++ Baseline_Code_V4/new_train.py
@@ -4,6 +4,7 @@ from utils import seed_everything
 import yaml
 
 from experiment import Experiment
+from inference import Inference
 import mlflow 
 import mlflow.pytorch 
 
@@ -37,26 +38,31 @@ if __name__ == "__main__":
 
     seed_everything(args.seed)
 
-    experiment = Experiment(args, margs)
-    tuner_params = nni.get_next_parameter()
-    
-    # -- mlflow
-    remote_server_uri = "http://101.101.211.226:30005"
-    mlflow.set_tracking_uri(remote_server_uri)
-    mlflow.set_experiment(args.experiment_name)
-    mlflow_experiment = mlflow.get_experiment_by_name(args.experiment_name)
-    client = mlflow.tracking.MlflowClient()
-    
-    run = client.create_run(mlflow_experiment.experiment_id)
-    
-    with mlflow.start_run(run_id=run.info.run_id): 
-        mlflow.set_tag("mlflow.user", args.user) 
-        mlflow.set_tag("mlflow.runName", args.run_name) 
+    if args.mode == "experiment":
+        experiment = Experiment(args, margs)
+        tuner_params = nni.get_next_parameter()
         
-        mlflow.log_params(dict(args))
-        mlflow.log_params(dict(margs_artifacts))
+        # -- mlflow
+        remote_server_uri = "http://101.101.211.226:30005"
+        mlflow.set_tracking_uri(remote_server_uri)
+        mlflow.set_experiment(args.experiment_name)
+        mlflow_experiment = mlflow.get_experiment_by_name(args.experiment_name)
+        client = mlflow.tracking.MlflowClient()
         
-        mlflow.log_dict(dict(args), args.run_name+"_args.yaml") 
-        mlflow.log_dict(dict(margs_artifacts), args.run_name+"_margs.yaml")  
+        run = client.create_run(mlflow_experiment.experiment_id)
         
-        experiment.run()
+        with mlflow.start_run(run_id=run.info.run_id): 
+            mlflow.set_tag("mlflow.user", args.user) 
+            mlflow.set_tag("mlflow.runName", args.run_name) 
+            
+            mlflow.log_params(dict(args))
+            mlflow.log_params(dict(margs_artifacts))
+            
+            mlflow.log_dict(dict(args), args.run_name+"_args.yaml") 
+            mlflow.log_dict(dict(margs_artifacts), args.run_name+"_margs.yaml")  
+            
+            experiment.run()
+            
+    elif args.mode == "inference":
+        inference = Inference(args, margs)
+        inference.run()
